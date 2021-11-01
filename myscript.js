b15a5fae9664b9b25c45cb558d4d2a5cbe47a4b2
Rei <reiyyan@gmail.com>
2021-11-01 19:08:36 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,13 @@
 $.ajax({
     method: "GET",
     url: "game-1.txt",
-    crossDomain: true,
 }).done(function (response) {
     $("#ajax-description").text(response);
+});
+
+$.ajax({
+    method: "GET",
+    url: "https://www.kingdomhearts.com/img/header/kh_trailer_still_img.jpg",
+}).done(function (response) {
+    $("#ajax-image").attr(src, response);
 });
