78102d9c97db3ac00054d17c87d9454e6fd526e8
Rei <reiyyan@gmail.com>
2021-11-01 19:54:54 -0400

    Added other games

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+// Start at 0
 $.ajax({
     method: "GET",
     url: "game_1.json",
@@ -9,3 +10,17 @@ $.ajax({
     });
     console.log($("#ajax-image"));
 });
+
+setTimeout(function(){
+    $.ajax({
+        method: "GET",
+        url: "game_2.json",
+    }).done(function (response) {
+        console.log(response);
+        $("#ajax-description").text(response.description);
+        $("#ajax-image").attr({
+            src: response.url,
+        });
+        console.log($("#ajax-image"));
+    });
+},5000); 
