7473eb2122e472ac2b7437e053106e17e4b3e7ed
Rei <reiyyan@gmail.com>
2021-11-01 19:26:57 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -8,6 +8,8 @@ $.ajax({
 $.ajax({
     method: "GET",
     url: "./images/kh3.jpg",
+    // success: 
 }).done(function (response) {
-    $("#ajax-image").attr('src', response);
+    // $("#ajax-image").attr('src', response);
+    $("#ajax-image").attr('src', 'data:image/png;base64,' + response);
 });
