33e5024d894d4d3f903e5f990a2f2f5b3040724d
Rei <reiyyan@gmail.com>
2021-11-01 19:56:21 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -17,4 +17,26 @@ setTimeout(function () {
         $("#ajax-description").text(response.description);
         $("#ajax-image").attr({ src: response.url });
     });
-}, 5000);
+}, 1000);
+
+setTimeout(function () {
+    $.ajax({
+        method: "GET",
+        url: "game_3.json",
+    }).done(function (response) {
+        console.log(response);
+        $("#ajax-description").text(response.description);
+        $("#ajax-image").attr({ src: response.url });
+    });
+}, 2000);
+
+setTimeout(function () {
+    $.ajax({
+        method: "GET",
+        url: "game_4.json",
+    }).done(function (response) {
+        console.log(response);
+        $("#ajax-description").text(response.description);
+        $("#ajax-image").attr({ src: response.url });
+    });
+}, 3000);
