e97779dec8a2ab8fa209a2637bff350be2cf8ecf
Rei <reiyyan@gmail.com>
2021-11-01 19:34:45 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,7 +2,6 @@ $.ajax({
     method: "GET",
     url: "game_1.json",
 }).done(function (response) {
-    game = JSON.parse(response);
-    $("#ajax-description").text(game.description);
-    $("#ajax-images").attr(src, game.url);
+    $("#ajax-description").text(response.description);
+    $("#ajax-images").attr(src, response.url);
 });
